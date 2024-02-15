49d838fa36b763055e479fb29345f8a0fc8f4d06
Cristian Florea <cristianflorea@cristians-macbook-air.home>
2024-02-15 16:17:36 +0100

    interacting with redux

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,28 @@
 "use client";
 import StoreProvider from "./StoreProvider";
-import { useAppSelector } from "@/lib/hooks";
+import { useAppSelector, useAppDispatch } from "@/lib/hooks";
+import { addTodo, toggleTodo } from "@/lib/features/todo";
 
 const List = () => {
   const todos = useAppSelector((state) => state.todos);
+  const dispatch = useAppDispatch();
 
   return (
     <div>
-      {todos.map((todo) => (
-        <li key={todo.id}>{todo.value}</li>
+      <button
+        onClick={() =>
+          dispatch(addTodo({ id: Math.random(), value: "4", completed: false }))
+        }
+      >
+        Add stuff
+      </button>
+      {todos.map((todo, index) => (
+        <li key={todo.id}>
+          {todo.value}
+          <button onClick={() => dispatch(toggleTodo(todo))}>
+            {todo.completed ? "done" : "pending"}
+          </button>
+        </li>
       ))}
     </div>
   );
--- src/lib/features/todo.ts
+++ src/lib/features/todo.ts
@@ -1,21 +1,30 @@
-import { createReducer } from "@reduxjs/toolkit";
+import { createReducer, createAction, current } from "@reduxjs/toolkit";
+
+const ADD_TODO = "ADD_TODO";
+const TOGGLE_TODO = "TOGGLE_TODO";
 
 const todosReducer = createReducer(
   [
-    { value: "todo 1", id: 1 },
-    { value: "todo 2", id: 2 },
-    { value: "todo 3", id: 3 },
+    { value: "todo 1", id: 1, completed: false },
+    { value: "todo 2", id: 2, completed: false },
+    { value: "todo 3", id: 3, completed: false },
   ],
   (builder) => {
     builder
-      .addCase("ADD_TODO", (state, action: any) => {
+      .addCase(ADD_TODO, (state, action: any) => {
         // "mutate" the array by calling push()
         state.push(action.payload);
+        return state;
       })
-      .addCase("TOGGLE_TODO", (state, action: any) => {
-        const todo = state[action.payload.index];
-        // "mutate" the object by overwriting a field
-        todo.completed = !todo.completed;
+      .addCase(TOGGLE_TODO, (state, action: any) => {
+        const nextItem = { ...action.payload };
+        nextItem.completed = !nextItem.completed;
+        return state.map((el, i) => {
+          if (el.id === action.payload.id) {
+            return nextItem;
+          }
+          return { ...el };
+        });
       })
       .addCase("REMOVE_TODO", (state, action: any) => {
         // Can still return an immutably-updated value if we want to
@@ -23,5 +32,8 @@ const todosReducer = createReducer(
       });
   }
 );
+
+export const addTodo = createAction(ADD_TODO);
+export const toggleTodo = createAction(TOGGLE_TODO);
 
 export default todosReducer;
